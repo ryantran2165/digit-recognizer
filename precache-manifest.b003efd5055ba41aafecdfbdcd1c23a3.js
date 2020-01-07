3f8b8fdcc320b2f921f35bc143ca9bd549a6cbe1
ryan_tran <ryantran2165@gmail.com>
2020-01-07 01:17:04 -0800

    Updates

---
+++ precache-manifest.b003efd5055ba41aafecdfbdcd1c23a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbd8d9ed0894e5ef117d3494e67fb16",
+    "url": "/handwritten-digits-neural-network/index.html"
+  },
+  {
+    "revision": "7e9258733ffdb619035c",
+    "url": "/handwritten-digits-neural-network/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c68b2fc1ea514b7f193d",
+    "url": "/handwritten-digits-neural-network/static/css/main.7b7db0d3.chunk.css"
+  },
+  {
+    "revision": "7e9258733ffdb619035c",
+    "url": "/handwritten-digits-neural-network/static/js/2.2df4a5a4.chunk.js"
+  },
+  {
+    "revision": "e25222ef4993b16ce4830064af33d09f",
+    "url": "/handwritten-digits-neural-network/static/js/2.2df4a5a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c68b2fc1ea514b7f193d",
+    "url": "/handwritten-digits-neural-network/static/js/main.9b00674a.chunk.js"
+  },
+  {
+    "revision": "ddf8ceb4c47afa326e4a",
+    "url": "/handwritten-digits-neural-network/static/js/runtime-main.4d87e3da.js"
+  }
+]);
